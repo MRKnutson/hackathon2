4a6dc568144d5fe2eb88fcb1deb8fad50f28f92a
Austin <austinchoi22@gmail.com>
2022-01-15 10:25:17 -0700

    pulling from main

--- client/src/pages/Locations.js
+++ client/src/pages/Locations.js
@@ -1,13 +1,45 @@
-import React, { useState } from "react"; 
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Locations = () => {
 
   const [locations, setLocations] = useState([]);
-  const []
+  const [appointments, setAppointments] = useState([]);
+
+  useEffect(() => {
+    getLocations();
+    getAppointments();
+  }, [])
+
+  const getLocations = async () => {
+    let res = await axios.get("/api/locations");
+    setLocations(res.data);
+    console.log(res.data)
+  }
+
+  const getAppointments = async () => {
+    let res = await axios.get("/api/appointments")
+    setAppointments(res.data);
+    console.log(res.data)
+  }
+
+  const renderAppointments = () => {
+    if (!appointments) {
+      return <p>Loading Appointments</p>
+    }
+    return (
+      <div>
+        <a href="/location/new">Add a Location</a>
+        <hr />
+        {renderAppointments()}
+        <hr />
+      </div>
+    )
+  };
 
   <div>
     <h1>Locations Page</h1>
   </div>
-}
+};
 
 export default Locations; 
