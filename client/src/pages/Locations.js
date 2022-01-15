82d0f4b3d35755eaf5cc82e7176dd77b429c1476
Austin <austinchoi22@gmail.com>
2022-01-15 09:54:26 -0700

    pulled down from main

---
+++ client/src/pages/Locations.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react"; 
+
+const Locations = () => {
+
+  const [locations, setLocations] = useState([]);
+  const []
+
+  <div>
+    <h1>Locations Page</h1>
+  </div>
+}
+
+export default Locations; 
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -5,28 +5,28 @@ import { AuthContext } from '../providers/AuthProvider';
 
 const Login = () => {
 
-  const {handleLogin} = useContext(AuthContext);
-  const [email, setEmail]=useState("test@test.com")
-  const [password, setPassword]=useState(123456)
+  const { handleLogin } = useContext(AuthContext);
+  const [email, setEmail] = useState("test@test.com")
+  const [password, setPassword] = useState(123456)
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    handleLogin({email, password}, navigate)
+    handleLogin({ email, password }, navigate)
   };
 
-  return(
+  return (
     <Container>
       <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email Address</Form.Label>
-          <Form.Control type="email" value = {email} placeholder="Enter Email" onChange = {(e)=>setEmail(e.target.value)}/>
+          <Form.Control type="email" value={email} placeholder="Enter Email" onChange={(e) => setEmail(e.target.value)} />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" value={password} placeholder="Password" onChange= {(e)=>setPassword(e.target.value)}/>
+          <Form.Control type="password" value={password} placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
         </Form.Group>
-        <Button type ="submit">Login</Button>
+        <Button type="submit">Login</Button>
       </Form>
     </Container>
   )
